a647e3b9408b451e6bd54d08667763651710061c
George <george-land@hotmail.com>
2017-07-20 18:19:52 +1000

    Left and right arrow keys now traverse between all variants.

--- EcoChess/Assets/Scripts/Managers/GameManager.cs
+++ EcoChess/Assets/Scripts/Managers/GameManager.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using ChessGameModes;
 
 public class GameManager : MonoBehaviour {
     public static GameManager Instance;
+    public static int modeIndex = 0;
     public Chess chessGame { get; private set; }
 
+    private const int NUM_OF_VARIANTS = 28;
     private bool gameFinished;
     private UIManager ui;
 
@@ -17,9 +21,20 @@ public class GameManager : MonoBehaviour {
             Instance = this;
         }
 
-        chessGame = new ChessGameModes.ChecklessChess();
+        chessGame = GameModeFactory.Create((GameMode)modeIndex);
+        //chessGame = new ChessGameModes.SovereignChess();
         chessGame.PopulateBoard();
         CenterCameraToBoard(chessGame.board);
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.RightArrow) && modeIndex != NUM_OF_VARIANTS - 1) {
+            modeIndex = Mathf.Clamp(++modeIndex, 0, NUM_OF_VARIANTS - 1);
+            SceneManager.LoadScene(0);
+        } else if (Input.GetKeyDown(KeyCode.LeftArrow) && modeIndex != 0) {
+            modeIndex = Mathf.Clamp(--modeIndex, 0, NUM_OF_VARIANTS - 1);
+            SceneManager.LoadScene(0);
+        }
     }
 
     public void OnTurnComplete() {
