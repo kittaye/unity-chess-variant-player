4129415685db885adbe4fe6b83ed3595648ad31e
George <george-land@hotmail.com>
2017-07-22 12:42:42 +1000

    Fully implemented fairy piece "SovereignPawn" (simply a pawn that behaves quite differently and only usable for variant "SovereignChess").

--- EcoChess/Assets/Scripts/Pieces/Fairy/SovereignPawn.cs
+++ EcoChess/Assets/Scripts/Pieces/Fairy/SovereignPawn.cs
@@ -5,29 +5,64 @@ using System.Collections.Generic;
 /// Sovereign Pawns are specific only to the Sovereign Chess variant and should not be used elsewhere.
 /// </summary>
 public class SovereignPawn : Pawn {
-    public SovereignPawn(Team team, BoardCoord position) : base(team, position) {
+    public enum Quadrant { BottomLeft, BottomRight, UpLeft, UpRight}
+    public Quadrant pieceQuadrant;
+
+    public SovereignPawn(Team team, BoardCoord position, Quadrant quadrant) : base(team, position) {
         m_pieceType = Piece.SovereignPawn;
+        this.pieceQuadrant = quadrant;
     }
-    public SovereignPawn(Team team, string algebraicKeyPosition) : base(team, algebraicKeyPosition) {
+    public SovereignPawn(Team team, string algebraicKeyPosition, Quadrant quadrant) : base(team, algebraicKeyPosition) {
         m_pieceType = Piece.SovereignPawn;
+        this.pieceQuadrant = quadrant;
     }
 
     public override string ToString() {
-        return "WHITE_Pawn";
+        return GetTeam() + "_Pawn";
     }
 
     public override List<BoardCoord> CalculateTemplateMoves() {
         List<BoardCoord> moves = new List<BoardCoord>();
-        if (MoveCount == 0) {
-            moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.UpRight, cap: initialMoveLimit, threatAttackLimit: 0));
-            moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.UpLeft, cap: initialMoveLimit, threatAttackLimit: 0));
-        } else {
-            moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.UpRight, cap: 1, threatAttackLimit: 0));
-            moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.UpLeft, cap: 1, threatAttackLimit: 0));
+
+        BoardCoord pos = GetBoardPosition();
+        uint moveCap = (pos.x <= 1 || pos.x >= 14 || pos.y <= 1 || pos.y >= 14) ? initialMoveLimit : 1;
+
+        switch (pieceQuadrant) {
+            case Quadrant.BottomLeft:
+                moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.Up, cap: moveCap, threatAttackLimit: 0));
+                moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.Right, cap: moveCap, threatAttackLimit: 0));
+
+                moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.UpRight, cap: 1, threatsOnly: true));
+                moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.UpLeft, cap: 1, threatsOnly: true));
+                moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.DownRight, cap: 1, threatsOnly: true));
+                break;
+            case Quadrant.BottomRight:
+                moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.Up, cap: moveCap, threatAttackLimit: 0));
+                moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.Left, cap: moveCap, threatAttackLimit: 0));
+
+                moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.UpRight, cap: 1, threatsOnly: true));
+                moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.UpLeft, cap: 1, threatsOnly: true));
+                moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.DownLeft, cap: 1, threatsOnly: true));
+                break;
+            case Quadrant.UpLeft:
+                moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.Down, cap: moveCap, threatAttackLimit: 0));
+                moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.Right, cap: moveCap, threatAttackLimit: 0));
+
+                moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.UpRight, cap: 1, threatsOnly: true));
+                moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.DownLeft, cap: 1, threatsOnly: true));
+                moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.DownRight, cap: 1, threatsOnly: true));
+                break;
+            case Quadrant.UpRight:
+                moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.Down, cap: moveCap, threatAttackLimit: 0));
+                moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.Left, cap: moveCap, threatAttackLimit: 0));
+
+                moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.UpLeft, cap: 1, threatsOnly: true));
+                moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.DownLeft, cap: 1, threatsOnly: true));
+                moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.DownRight, cap: 1, threatsOnly: true));
+                break;
+            default:
+                break;
         }
-
-        moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.Up, cap: 1, threatsOnly: true));
-
         return moves;
     }
 }
