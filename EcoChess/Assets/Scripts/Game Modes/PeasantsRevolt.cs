14baad1296b71488b1ceb9d59ce4f9bf56d8a747
George <george-land@hotmail.com>
2017-07-22 12:57:22 +1000

    Fixed "PeasantsRevolt" variant's initial pieces.

--- EcoChess/Assets/Scripts/Game Modes/PeasantsRevolt.cs
+++ EcoChess/Assets/Scripts/Game Modes/PeasantsRevolt.cs
@@ -8,8 +8,8 @@ namespace ChessGameModes {
     /// Winstate: Checkmate.
     /// Piece types: Orthodox.
     /// Board layout:
-    ///     . n n . k n n .
-    ///     p p p p p p p p
+    ///     . n n . k . n .
+    ///     . . . . p . . .
     ///     . . . . . . . .
     ///     . . . . . . . .
     ///     . . . . . . . .
@@ -28,11 +28,12 @@ namespace ChessGameModes {
             currentRoyalPiece = (King)AddPieceToBoard(new King(Team.WHITE, new BoardCoord(4, WHITE_BACKROW)));
             opposingRoyalPiece = (King)AddPieceToBoard(new King(Team.BLACK, new BoardCoord(4, BLACK_BACKROW)));
 
+            AddPieceToBoard(new Pawn(Team.BLACK, new BoardCoord(4, BLACK_PAWNROW)));
+
             for (int x = 0; x < BOARD_WIDTH; x++) {
                 AddPieceToBoard(new Pawn(Team.WHITE, new BoardCoord(x, WHITE_PAWNROW)));
-                AddPieceToBoard(new Pawn(Team.BLACK, new BoardCoord(x, BLACK_PAWNROW)));
 
-                if (x == 1 || x == 2 || x == BOARD_WIDTH - 2 || x == BOARD_WIDTH - 3) {
+                if (x == 1 || x == 2 || x == BOARD_WIDTH - 2) {
                     AddPieceToBoard(new Knight(Team.BLACK, new BoardCoord(x, BLACK_BACKROW)));
                 }
             }
