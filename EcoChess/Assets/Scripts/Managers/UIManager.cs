142d3127853b09c517f4b8d59c3f3fcc7e4c45d8
George <george-land@hotmail.com>
2017-07-22 12:51:16 +1000

    UI Manager now creates promotion options and other relevant UI. Now listens to events OnDisplayPromotionOptions and OnPawnPromotionOptionsChanged, that are invoked from the chess variant. Clicking a promotion option will cause that option to be the newly selected pawn promotion. When a turn is complete, the promotion options will change teams to match the current team.

--- EcoChess/Assets/Scripts/Managers/UIManager.cs
+++ EcoChess/Assets/Scripts/Managers/UIManager.cs
@@ -6,9 +6,13 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour {
     public static UIManager Instance;
     public Canvas mainCanvas;
+    public GameObject promotionWindow;
+    public GameObject promoterPrefab;
 
     private Text teamTurnLbl;
     private Text gameModeLbl;
+    private Text promoteToLbl;
+    private List<GameObject> promotionOptions;
 
     // Use this for initialization
     void Awake () {
@@ -18,6 +22,10 @@ public class UIManager : MonoBehaviour {
             Instance = this;
         }
 
+        promotionOptions = new List<GameObject>();
+        ChessGameModes.FIDERuleset._DisplayPromotionUI += OnDisplayPromotionOptions;
+        ChessGameModes.FIDERuleset._OnPawnPromotionsChanged += OnPawnPromotionOptionsChanged;
+
         foreach (Text aText in mainCanvas.GetComponentsInChildren<Text>()) {
             if (aText.name.Equals("TeamTurn_lbl")) {
                 teamTurnLbl = aText;
@@ -25,7 +33,49 @@ public class UIManager : MonoBehaviour {
             if (aText.name.Equals("GameMode_lbl")) {
                 gameModeLbl = aText;
             }
+            if (aText.name.Equals("PromoteTo_lbl")) {
+                promoteToLbl = aText;
+            }
         }
+
+        OnDisplayPromotionOptions(false);
+    }
+
+    void OnDestroy() {
+        ChessGameModes.FIDERuleset._DisplayPromotionUI -= OnDisplayPromotionOptions;
+        ChessGameModes.FIDERuleset._OnPawnPromotionsChanged -= OnPawnPromotionOptionsChanged;
+    }
+
+    public void CreatePawnPromotionOptions(Piece[] pieces) {
+        promotionOptions.Clear();
+        Button[] buttons = promotionWindow.GetComponentsInChildren<Button>();
+        for (int i = 0; i < buttons.Length; i++) {
+            buttons[i].onClick.RemoveListener(() => SelectPawnPromotion((Piece)System.Enum.Parse(typeof(Piece), buttons[i].name)));
+            Destroy(buttons[i].gameObject);
+        }
+
+        for (int i = 0; i < pieces.Length; i++) {
+            GameObject go = Instantiate(promoterPrefab, promotionWindow.transform);
+            go.name = pieces[i].ToString();
+            go.GetComponent<Image>().sprite = Resources.Load<Sprite>(GameManager.Instance.chessGame.GetCurrentTeamTurn().ToString() + "_" + go.name);
+            int j = i;
+            go.GetComponent<Button>().onClick.AddListener(() => SelectPawnPromotion(pieces[j]));
+            promotionOptions.Add(go);
+        }
+    }
+
+    public void OnDisplayPromotionOptions(bool value) {
+        promotionWindow.SetActive(value);
+    }
+
+    public void OnPawnPromotionOptionsChanged(Piece[] pieces) {
+        promoteToLbl.text = "<color=white>Promote to:\n</color><b>" + ((ChessGameModes.FIDERuleset)GameManager.Instance.chessGame).selectedPawnPromotion.ToString() + "</b>";
+        CreatePawnPromotionOptions(pieces);
+    }
+
+    public void SelectPawnPromotion(Piece value) {
+        promoteToLbl.text = "<color=white>Promote to:\n</color><b>" + value.ToString() + "</b>";
+        ((ChessGameModes.FIDERuleset)GameManager.Instance.chessGame).SetPawnPromotionTo(value);
     }
 
     void Start() {
@@ -37,13 +87,17 @@ public class UIManager : MonoBehaviour {
     }
 
     public void OnTurnComplete() {
-        if(GameManager.Instance.chessGame.GetCurrentTeamTurn() == Team.WHITE) {
+        Team newCurrentTeam = GameManager.Instance.chessGame.GetCurrentTeamTurn();
+        if (newCurrentTeam == Team.WHITE) {
             teamTurnLbl.text = "White's move";
             teamTurnLbl.color = Color.white;
         } else {
             teamTurnLbl.text = "Black's move";
             teamTurnLbl.color = Color.black;
         }
+
+        foreach (GameObject item in promotionOptions) {
+            item.GetComponent<Image>().sprite = Resources.Load<Sprite>(newCurrentTeam.ToString() + "_" + item.name);
+        }
     }
-	
 }
