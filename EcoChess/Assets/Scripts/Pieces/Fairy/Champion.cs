0c28b8974cd790f92ea3f92010bf6d0c6d5dc09c
George <george-land@hotmail.com>
2017-07-30 15:00:45 +1000

    Created temporary sprites for fairy pieces "Wizard" and "Champion". Also fully implemented these pieces.

---
+++ EcoChess/Assets/Scripts/Pieces/Fairy/Champion.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Champion : ChessPiece {
+    public Champion(Team team, BoardCoord position) : base(team, position) {
+        m_pieceType = Piece.Champion;
+    }
+    public Champion(Team team, string algebraicKeyPosition) : base(team, algebraicKeyPosition) {
+        m_pieceType = Piece.Champion;
+    }
+
+    public override string ToString() {
+        return GetTeam() + "_Champion";
+    }
+
+    public override List<BoardCoord> CalculateTemplateMoves() {
+        List<BoardCoord> moves = new List<BoardCoord>();
+
+        moves.AddRange(chessGame.TryGetCustomDirectionalMoves(this, 0, 1, cap: 2));
+        moves.AddRange(chessGame.TryGetCustomDirectionalMoves(this, 0, -1, cap: 2));
+        moves.AddRange(chessGame.TryGetCustomDirectionalMoves(this, 1, 0, cap: 2));
+        moves.AddRange(chessGame.TryGetCustomDirectionalMoves(this, -1, 0, cap: 2));
+
+        moves.AddRange(chessGame.TryGetCustomDirectionalMoves(this, 2, 2, cap: 1));
+        moves.AddRange(chessGame.TryGetCustomDirectionalMoves(this, -2, 2, cap: 1));
+        moves.AddRange(chessGame.TryGetCustomDirectionalMoves(this, 2, -2, cap: 1));
+        moves.AddRange(chessGame.TryGetCustomDirectionalMoves(this, -2, -2, cap: 1));
+
+        return moves;
+    }
+}
---
+++ EcoChess/Assets/Scripts/Pieces/Fairy/Wizard.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Wizard : ChessPiece {
+    public Wizard(Team team, BoardCoord position) : base(team, position) {
+        m_pieceType = Piece.Wizard;
+    }
+    public Wizard(Team team, string algebraicKeyPosition) : base(team, algebraicKeyPosition) {
+        m_pieceType = Piece.Wizard;
+    }
+
+    public override string ToString() {
+        return GetTeam() + "_Wizard";
+    }
+
+    public override List<BoardCoord> CalculateTemplateMoves() {
+        List<BoardCoord> moves = new List<BoardCoord>();
+
+        moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.UpLeft, cap: 1));
+        moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.UpRight, cap: 1));
+        moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.DownLeft, cap: 1));
+        moves.AddRange(chessGame.TryGetDirectionalMoves(this, MoveDirection.DownRight, cap: 1));
+
+        // Vertical "L" movements
+        moves.AddRange(chessGame.TryGetCustomDirectionalMoves(this, 1, 3, cap: 1));
+        moves.AddRange(chessGame.TryGetCustomDirectionalMoves(this, -1, 3, cap: 1));
+        moves.AddRange(chessGame.TryGetCustomDirectionalMoves(this, 1, -3, cap: 1));
+        moves.AddRange(chessGame.TryGetCustomDirectionalMoves(this, -1, -3, cap: 1));
+
+        // Horizontal "L" movements
+        moves.AddRange(chessGame.TryGetCustomDirectionalMoves(this, 3, 1, cap: 1));
+        moves.AddRange(chessGame.TryGetCustomDirectionalMoves(this, -3, 1, cap: 1));
+        moves.AddRange(chessGame.TryGetCustomDirectionalMoves(this, 3, -1, cap: 1));
+        moves.AddRange(chessGame.TryGetCustomDirectionalMoves(this, -3, -1, cap: 1));
+
+        return moves;
+    }
+}
