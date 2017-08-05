155b3c87cb9ff5ad36d4384c779f872943cc15ad
George <george-land@hotmail.com>
2017-08-05 23:33:51 +1000

    FIDE and all sub variants now re-set the selected pawn promotion option to Queen. Now virtual support for "SetPawnPromotionTo" method to allow a variant to perform additional functions after changing pawn promotion options.

--- EcoChess/Assets/Scripts/Game Modes/FIDERuleset.cs
+++ EcoChess/Assets/Scripts/Game Modes/FIDERuleset.cs
@@ -102,6 +102,7 @@ namespace ChessGameModes {
         public override void OnTurnComplete() {
             base.OnTurnComplete();
 
+            selectedPawnPromotion = Piece.Queen;
             ChessPiece temp = currentRoyalPiece;
             currentRoyalPiece = opposingRoyalPiece;
             opposingRoyalPiece = temp;
@@ -254,7 +255,7 @@ namespace ChessGameModes {
             if (_DisplayPromotionUI != null) _DisplayPromotionUI.Invoke(true);
         }
 
-        public void SetPawnPromotionTo(Piece piece) {
+        public virtual void SetPawnPromotionTo(Piece piece) {
             this.selectedPawnPromotion = piece;
         }
 
