23794dab729db450fafd8574c8cc7a08647799fb
George Delosa <georgedel1@outlook.com>
2018-08-10 14:23:48 +1000

    Refactoring - removed redundant code

--- EcoChess/Assets/Scripts/Game Modes/ActiveChess.cs
+++ EcoChess/Assets/Scripts/Game Modes/ActiveChess.cs
@@ -27,29 +27,12 @@ namespace ChessGameModes {
         }
 
         public override void PopulateBoard() {
-            currentRoyalPiece = (King)AddPieceToBoard(new King(Team.WHITE, new BoardCoord(4, WHITE_BACKROW)));
-            opposingRoyalPiece = (King)AddPieceToBoard(new King(Team.BLACK, new BoardCoord(4, BLACK_BACKROW)));
+            base.PopulateBoard();
 
-            aSideWhiteRook = (Rook)AddPieceToBoard(new Rook(Team.WHITE, new BoardCoord(0, WHITE_BACKROW)));
-            aSideBlackRook = (Rook)AddPieceToBoard(new Rook(Team.BLACK, new BoardCoord(0, BLACK_BACKROW)));
-            hSideWhiteRook = (Rook)AddPieceToBoard(new Rook(Team.WHITE, new BoardCoord(7, WHITE_BACKROW)));
-            hSideBlackRook = (Rook)AddPieceToBoard(new Rook(Team.BLACK, new BoardCoord(7, BLACK_BACKROW)));
-
-            for (int x = 0; x < BOARD_WIDTH; x++) {
-                AddPieceToBoard(new Pawn(Team.WHITE, new BoardCoord(x, WHITE_PAWNROW)));
-                AddPieceToBoard(new Pawn(Team.BLACK, new BoardCoord(x, BLACK_PAWNROW)));
-
-                if (x == 1 || x == BOARD_WIDTH - 3) {
-                    AddPieceToBoard(new Knight(Team.WHITE, new BoardCoord(x, WHITE_BACKROW)));
-                    AddPieceToBoard(new Knight(Team.BLACK, new BoardCoord(x, BLACK_BACKROW)));
-                } else if (x == 2 || x == BOARD_WIDTH - 4) {
-                    AddPieceToBoard(new Bishop(Team.WHITE, new BoardCoord(x, WHITE_BACKROW)));
-                    AddPieceToBoard(new Bishop(Team.BLACK, new BoardCoord(x, BLACK_BACKROW)));
-                } else if (x == 3 || x == BOARD_WIDTH - 1) {
-                    AddPieceToBoard(new Queen(Team.WHITE, new BoardCoord(x, WHITE_BACKROW)));
-                    AddPieceToBoard(new Queen(Team.BLACK, new BoardCoord(x, BLACK_BACKROW)));
-                }
-            }
+            AddPieceToBoard(new Pawn(Team.WHITE, new BoardCoord(BOARD_WIDTH - 1, WHITE_PAWNROW)));
+            AddPieceToBoard(new Pawn(Team.BLACK, new BoardCoord(BOARD_WIDTH - 1, BLACK_PAWNROW)));
+            AddPieceToBoard(new Queen(Team.WHITE, new BoardCoord(BOARD_WIDTH - 1, WHITE_BACKROW)));
+            AddPieceToBoard(new Queen(Team.BLACK, new BoardCoord(BOARD_WIDTH - 1, BLACK_BACKROW)));
         }
     }
 }
